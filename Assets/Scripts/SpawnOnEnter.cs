75acf02790e01dca3c42a87fff4c269b920fb65e
iiyuu16 <pcg4m3r16@gmail.com>
2024-06-25 11:56:33 +0800

    v1.031
    added level generation upon entry
    
    - modify conditionscript to disable selected colliders and enable bridges

---
+++ Assets/Scripts/SpawnOnEnter.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnEnter : MonoBehaviour
+{
+    public GameObject[] gameObjectsToEnable;
+    public GameObject[] gameObjectsToDisable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Optionally, you can initialize or setup anything here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If needed, you can use this for other update logic
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (GameObject obj in gameObjectsToEnable)
+            {
+                obj.SetActive(true);
+            }
+
+            foreach (GameObject obj in gameObjectsToDisable)
+            {
+                obj.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (GameObject obj in gameObjectsToEnable)
+            {
+                obj.SetActive(false);
+            }
+
+            foreach (GameObject obj in gameObjectsToDisable)
+            {
+                obj.SetActive(true);
+            }
+        }
+    }
+}
